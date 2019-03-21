be090c581ccd87de5d62da6617efccd0d4ec089f
AlexSR <Wincf@yandex.ru>
2019-03-21 16:00:30 +0300

    12.03.2019

--- pythonTask_1.4.py
+++ pythonTask_1.4.py
@@ -1,109 +1,90 @@
 n = int(input())
-sl = {}
+sl = {'command': None, 'namespace': None, 'parent': None, 'var': None}
 sp = []
+
+def create():
+    global command,namespace,parOrVar
+    global sl
+    sp_cr = []
+    if sl.get('command')!=None:
+        sp_cr = sl.get('command')
+        sp_cr.append(command)
+        sl['command'] = sp_cr
+        sp_cr = []
+        sp_cr = sl.get('namespace')
+        sp_cr.append(namespace)
+        sl['namespace'] = sp_cr
+        sp_cr = []
+        sp_cr = sl.get('var')
+        sp_cr.append(parOrVar)
+        sl['var'] = sp_cr
+        sp_cr = []
+        sp_cr = sl.get('parent')
+        sp_cr.append('None')
+        sl['parent'] = sp_cr
+        sp_cr = []
+def add():
+    global command,namespace,parOrVar
+    global sl
+    sp_cr = []
+    sp_cr = sl.get('command')
+    sp_cr.append(command)
+    sl['command'] = sp_cr
+    sp_cr = []
+    sp_cr = sl.get('namespace')
+    sp_cr.append(namespace)
+    sl['namespace'] = sp_cr
+    sp_cr = []
+    sp_cr = sl.get('var')
+    sp_cr.append('None')
+    sl['var'] = sp_cr
+    sp_cr = []
+    sp_cr = sl.get('parent')
+    sp_cr.append(parOrVar)
+    sl['parent'] = sp_cr
+    sp_cr = []
+def poisk(namespace,pos):
+    global sl
+    for i in range(sl.get('namespace')):
+        if namespace==sl.get('namespace')[i] and sl.get('command')[i] == 'create':
+
 def get_func():
     global command,namespace,parOrVar
     global sl
     mas = sl.get('var')
-    sp_var  = []
-    sp_namespace = []
+    sp_num = []
+    sp_ns = []
     flag = False
+    rez = ''
     for i in range(len(mas)):
-        if parOrVar==mas[i]:
-            sp_var.append(i)
-    if len(sp_var)!=0:
-        for i in range(len(sp_var)):
-            if namespace ==sl.get('namespace')[sp_var[i]]:
+        if mas[i]==parOrVar:
+            sp_num.append(i)
+    if len(sp_num)>1:
+        for i in range(len(sp_num)):
+            if sl.get('namespace')[sp_num[i]] == namespace:
                 flag = True
+                rez = namespace
                 break
-        if flag:
-            return namespace
-        else:
-            for j in range(len(sl.get('namespace'))):
-                if sl.get('namespace')[j]==namespace and sl.get('command')[j]=='create':
-                    flag = True
-                    break
-            if flag:
-                return sl.get('parent')[j]
             else:
-                return 'None'
-    else:
-        return 'None'
+                sp_ns = sl.get('namespace')
+
+
+    elif len(sp_num)==1:
+        if sl.get('namespace')[sp_num[0]] == namespace:
+            flag = True
+            rez = namespace
+        else:
+
+
+
 if 1<=n<=100:
     for i in range(n):
-        command,namespace,parOrVar = input().split()
-        if command!='get':
-            if sl.get('command')!=None:
-                mas = sl.get('command')
-                for i in range(len(mas)):
-                    sp.append(mas[i])
-                sp.append(command)
-                a=0
-                sl['command']=sp
-                sp=[]
-            else:
-                sp.append(command)
-                sl['command'] = sp
-                sp = []
-            if sl.get('namespace')!=None:
-                mas = sl.get('namespace')
-                for i in range(len(mas)):
-                    sp.append(mas[i])
-                sp.append(namespace)
-                sl['namespace']=sp
-                sp=[]
-            else:
-                sp.append(namespace)
-                sl['namespace'] = sp
-                sp = []
-            if command=='create':
-                if sl.get('parent')!=None:
-                    mas = sl.get('parent')
-                    for i in range(len(mas)):
-                        sp.append(mas[i])
-                    sp.append(parOrVar)
-                    sl['parent']=sp
-                    sp=[]
-                else:
-                    sp.append(parOrVar)
-                    sl['parent'] = sp
-                    sp = []
-                if sl.get('var')!=None:
-                    mas = sl.get('var')
-                    for i in range(len(mas)):
-                        sp.append(mas[i])
-                    sp.append('clear')
-                    sl['var']=sp
-                    sp=[]
-                else:
-                    sp.append('clear')
-                    sl['var'] = sp
-                    sp = []
-            if command=='add':
-                if sl.get('var')!=None:
-                    mas = sl.get('var')
-                    for i in range(len(mas)):
-                        sp.append(mas[i])
-                    sp.append(parOrVar)
-                    sl['var']=sp
-                    sp=[]
-                else:
-                    sp.append(parOrVar)
-                    sl['var'] = sp
-                    sp = []
-                if sl.get('parent')!=None:
-                    mas = sl.get('parent')
-                    for i in range(len(mas)):
-                        sp.append(mas[i])
-                    sp.append('clear')
-                    sl['parent']=sp
-                    sp=[]
-                else:
-                    sp.append('clear')
-                    sl['parent'] = sp
-                    sp = []
-        else:
-            print(get_func())
-
+        command, namespace, parOrVar = input().split()
+        if command == 'create':
+            create()
+        elif command=='add':
+            add()
+        elif command == 'get':
+            get_func()
 else:
     print('Error')
