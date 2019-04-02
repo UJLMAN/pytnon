c6f1c1a5eb5f5f04153f03f168308051a7eaedfb
AlexSR <Wincf@yandex.ru>
2019-04-02 16:34:09 +0300

    02.04.2019

---
+++ Task_1.5_2.py
@@ -0,0 +1,30 @@
+class Buffer:
+    def __init__(self):
+        self.spis = []
+
+    def add(self, *a):
+        sum = 0
+        ip  = 0
+        sp = []
+        for i in range(len(a)):
+            self.spis.append(a[i])
+        if len(self.spis)>5:
+            for i in range(len(self.spis)):
+                sum+=self.spis[i]
+                if (i+1)%5==0:
+                    print(sum)
+                    sum = 0
+                    ip = i+1
+            for i in range(ip,len(self.spis)):
+               sp.append(self.spis[i])
+            self.spis = sp
+
+    def get_current_part(self):
+        print(*self.spis)
+x = Buffer()
+x.add(1,2,3)
+x.get_current_part()
+x.add(4,5,6)
+x.get_current_part()
+x.add(7,8,9,10)
+x.get_current_part()
